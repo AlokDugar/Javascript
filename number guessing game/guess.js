23c9865b911d39b7634047b241fb9061258364c5
AlokDugar <alokdugar4@gmail.com>
2024-01-16 16:34:38 +0545

    number guessing

---
+++ number guessing game/guess.js
@@ -0,0 +1,23 @@
+
+let r=Math.floor(Math.random()*100);
+let f;
+for(let i=1;i<=3;i++){
+    let g;
+    setTimeout( g=parseInt(prompt("Enter your guess")),4000);
+    if(r===g){
+        console.log("Guessed");
+        f=1
+    }
+    if(r<g){
+        console.log("High");
+    }
+    else{
+        console.log("Low");
+    }
+}
+if(f==1){
+    console.log("Number Guessed")
+}
+else{
+    console.log("Number not found")
+}
