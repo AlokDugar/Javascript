443986315cfc1868efb38728ca4b93f41e369cb1
AlokDugar <alokdugar4@gmail.com>
2024-01-17 14:27:55 +0545

    commit

--- object.js
+++ object.js
@@ -4,6 +4,7 @@ const person = {
   add: 'Sano Gaucharan',
   age: 20,
   does_Alok_exist: true,
+  hello:(n)=>console.log("Hi! My name is "+n)
 };
 console.log(person);
 console.log('Name: ' + person['name']);
@@ -13,3 +14,4 @@ console.log('Age: ' + person['age']);
 console.log(person['does_Alok_exist']);
 console.log(person['college_name']);
 console.log('Name:'+person.name)
+person.hello(person.name)
---
+++ this.js
@@ -0,0 +1,9 @@
+const person = {
+    name: 'Alok Dugar',
+    pn: 9804706675,
+    add: 'Sano Gaucharan',
+    age: 20,
+    does_Alok_exist: true,
+    hello:()=>console.log(`Hi! My name is ${this.name}`)
+  };
+person.hello()
